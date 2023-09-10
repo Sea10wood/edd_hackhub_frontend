3c3e16aa6562eeda35b8b0a55e7998fca12d6896
claustra01 <shinya430.g@gmail.com>
2023-09-10 13:06:56 +0900

    update: user my profile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -4,11 +4,49 @@ import { getWindowSize } from "@/hooks/getWindowsize";
 import { Box, Button, Card, Typography } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { axiosBaseURL } from ".";
+import { useRouter } from "next/router";
 
 const Profile = () => {
-    const user_name = "Sea10wood"; // GitHubのユーザー名をここに指定
+
+    const router = useRouter();
     const { height, width } = getWindowSize();
+
+    const [name, setName] = useState<string>('');
+    const [desc, setDesc] = useState<string>('');
+    const [iconUrl, setIconUrl] = useState<string>('');
+
+    const getUserData = async (token: string) => {
+        try {
+            const response = await axios.get(`${axiosBaseURL}/api/users`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+            })
+            if (response.status === 200) {
+                setName(response.data.name);
+                setDesc(response.data.description);
+                setIconUrl(response.data.iconUrl);
+            } else {
+                console.error(response.statusText)
+                router.push('/')
+            }
+        } catch (error) {
+            console.error(error)
+            router.push('/')
+        }
+    }
+
+    useEffect(() => {
+        const token = sessionStorage.getItem('token');
+        if (token !== null) {
+            getUserData(token);
+        }
+    }, []);
+
     return (
         <>
             <Typography
@@ -44,7 +82,7 @@ const Profile = () => {
                 }}
             >
                 <Image
-                    src={`https://github.com/${user_name}.png`}
+                    src={`https://github.com/${name}.png`}
                     height={50}
                     width={50}
                     alt={"githubアイコン"}
@@ -97,7 +135,7 @@ const Profile = () => {
                 }}
                 variant="h4"
             >
-                {user_name}のGitHubプロフィール
+                {name}のGitHubプロフィール
             </Typography>
 
             <Card
@@ -109,7 +147,7 @@ const Profile = () => {
                     height: "80%",
                 }}
             >
-                <Link href={`https://github.com/${user_name}`}>
+                <Link href={`https://github.com/${name}`}>
                     <Button
                         variant="contained"
                         sx={{
@@ -131,8 +169,8 @@ const Profile = () => {
                         left: "30%",
                     }}
                 >
-                    <GitHubStatsCard />
-                    <GithubMostLanguage />
+                    <GitHubStatsCard name={name} />
+                    <GithubMostLanguage name={name} />
                 </div>
             </Card>
         </>
