befb342a73a4033ec1d640131b180f5b9425cfd9
Sea10wood <miyusea5@gmail.com>
2023-10-14 09:42:33 +0900

    roomの修正

--- src/pages/rooms.tsx
+++ src/pages/rooms.tsx
@@ -1,48 +1,49 @@
-import { Box, Button, Typography } from "@mui/material"
+import { Box, Button, Typography } from "@mui/material";
 import Image from "next/image";
-import React, { useState } from 'react';
-import CreateEventModal from "@/components/eventinputmodal";
+import React, { useState } from "react";
 import Link from "next/link";
 import Sidebar from "@/components/layout";
 import { useRouter } from "next/router";
+import CategoryCard from "@/components/CategoryCard";
+import { useEventContext } from "@/contexts/EventContext";
 
-
+type EventData = {
+  title: string;
+  url: string;
+  description: string;
+  deadline: string;
+  day: string;
+};
 const Room = () => {
-    const router = useRouter();
-    const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const router = useRouter();
+  const { eventTitles } = useEventContext();
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const initialEvents: EventData[] = [
+    {
+      title: "EDDハッカソン",
+      url: "",
+      description: "",
+      deadline: "",
+      day: "",
+    },
+  ];
 
-    const handleOpenModal = () => {
-        setModalOpen(true);
-    };
+  const [eventTitle, setEventTitle] = useState(initialEvents);
 
-    const handleCloseModal = () => {
-        setModalOpen(false);
-    };
+  const handleCreateEvent = (eventUrl: any) => {
+    console.log("新しいイベントが作成されました:", eventTitle);
+  };
 
-    const handleCreateEvent = (eventUrl: any) => {
-        console.log('新しいイベントが作成されました:', eventUrl);
-    };
-    const eventData = {
-        title: "イベントのタイトル",
-        description: "イベントの説明文",
-        url: "/event/1",
-    };
-    return (
-        <>
-            <>
-               <Sidebar/>
-
-
-            </>
-
-
-        </>
-
-
-    );
+  return (
+    <>
+      <Sidebar />
+      <div style={{ marginTop: "100px", marginLeft: "140px" }}>
+        {eventTitle.map((event, index) => (
+          <CategoryCard key={index} event={event} />
+        ))}
+      </div>
+    </>
+  );
 };
 
-
 export default Room;
-
-
